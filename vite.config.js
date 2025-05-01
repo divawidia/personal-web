90b1725a8e5b9ed58491df6c02ffe16342515154
divawidia <wiartha2001@gmail.com>
2025-05-01 11:43:19 +0800

    refactor: update vit cofig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/site.scss', 'resources/js/common.js'],
+            input: ["resources/sass/site.scss", "resources/js/site.js"],
             refresh: true,
         }),
         tailwindcss(),
