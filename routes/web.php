35b453127d2f2a083c7e62f8f5c8c12a753bcf6e
divawidia <wiartha2001@gmail.com>
2025-05-01 16:41:25 +0800

    feat: create sitemap generator sitemap

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::get('/generate-sitemap', [SitemapController::class, 'generateSitemap']);
